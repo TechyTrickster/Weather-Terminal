a304ba7165cf9719b97756e28894b2adbff746df
TechyTrickster <33636686+TechyTrickster@users.noreply.github.com>
2021-01-06 18:39:53 -0600

    added adjustable limits to line widths

--- getWeather.sh
+++ getWeather.sh
@@ -3,11 +3,20 @@
 #process input and retrieve national weather service page for the given location
 City="$(echo "$1" | tr ' ' '+')"
 State=$2
+width=110
+NC='\033[0m'
+RED='\033[0;31m'
+BLUE='\033[0;34m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+CYAN='\033[0;36m'
+
 searchQuery="https://forecast.weather.gov/MapClick.php?CityName=$City&state=$State"
 inputPage="temp.http"
 wget -q "$searchQuery" -O $inputPage
 
-seperator="$(yes '-' | head -n 120 | tr -d '\n')"
+seperatorA="$(yes '-' | head -n $width | tr -d '\n')"
+seperatorB="$(yes '=' | head -n $width | tr -d '\n')"
 HumidityRaw="$(grep -A1 "Humidity" $inputPage | sed '1d')"
 VisibilityRaw="$(grep -A1 "Visibility" $inputPage | sed '1d')"
 WindChillRaw="$(grep "Wind Chill" $inputPage)"
@@ -22,15 +31,14 @@ TimeRaw="$(grep -A2 "Last update" $inputPage | sed '1,2d')"
 #format raw string and output them to the user
 echo "Weather Report As Of" | toilet -f pagga
 echo "$TimeRaw" | sed -e 's/^[[:space:]]*//g' | sed 's/\<td\>//g' | tr -d '<>/' | sed -e 's/[[:space:]]*$//g' | toilet -f pagga
-echo "$SkyRaw" | sed -e 's/^[[:space:]]*//g' | sed 's/.*current//g' | tr -d '<>/p\"' | toilet -w 100 -f pagga
+echo "$SkyRaw" | sed -e 's/^[[:space:]]*//g' | sed 's/.*current//g' | tr -d '<>/p\"' | toilet -w $width -f pagga
+echo "Temperature of $(echo "$TemperatureRaw" | sed 's/.*sm//g' | tr -d 'p<>deg&/\"' | tr ';' ' ')" | toilet -f pagga
 echo "Humidity:    " | tr -d '\n'
 echo "$HumidityRaw" | sed -e 's/^[[:space:]]*//g' | tr -d 'td<>/'
 echo "Wind Speed:  " | tr -d '\n'
 echo "$WindSpeedRaw" | sed -e 's/^[[:space:]]*//g' | tr -d 'td<>/'
 echo "Barometer:   " | tr -d '\n'
 echo "$BarometerRaw" | sed -e 's/^[[:space:]]*//g' | tr -d 'td<>/'
-echo "Temperature: " | tr -d '\n'
-echo "$TemperatureRaw" | sed 's/.*sm//g' | tr -d 'p<>deg&/\"' | tr ';' ' '
 echo "Visibility:  " | tr -d '\n'
 echo "$VisibilityRaw: " | sed -e 's/^[[:space:]]*//g' | tr -d 'td<>/:'
 echo "Wind Chill:  " | tr -d '\n'
@@ -43,7 +51,7 @@ echo
 echo
 toilet "Detailed Forecast" -f pagga
 forecastWhole="$(grep -o "title=.*\" " $inputPage | sed '1d' | sed 's/title.//g' | sed '10,$d' | tr -d '\"')"
-
+echo "$seperatorB"
 
 for i in {1..9}
 do
@@ -51,9 +59,11 @@ do
 	description="d$i.txt"
 	sedLine="$(echo "$i p" | tr -d ' ')"
 	echo "$forecastWhole" | sed -n "$sedLine" | grep -o ".*:" | toilet -f future > $period
-	echo "$forecastWhole" | sed -n "$sedLine" | sed 's/.*://g' | fold -s -w 80 > $description
+	length=$(wc -L $period | sed 's/ .*//g')
+	let "size = width - length - 1"
+	echo "$forecastWhole" | sed -n "$sedLine" | sed 's/.*://g' | fold -s -w $size > $description
 	paste $period $description | column -s $'\t' -t
-	echo "$seperator"
+	echo "$seperatorA"
 	rm $period $description
 done
 
